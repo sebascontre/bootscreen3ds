6ffb97ae6fd5f7b1e71904e984d0d118727b79b4
Sebastián Contreras <seba.contreras@outlook.com>
2017-02-20 16:53:52 +0000

    Fix Internal Memory Selector and add Luma3DS Hourly option

--- scripts.js
+++ scripts.js
@@ -126,7 +126,7 @@ $("#settings input, #settings select").on('change', function() {
 			break;
 	}
 
-	write(0, 16*9,  'Detecting Primary Master ... 2G Internal Memory');
+	write(0, 16*9,  'Detecting Primary Master ... '+ processor/2 +'G Internal Memory');
 	write(0, 16*10, 'Detecting Primary Slave  ... '+sd+' Card');
 	
 	if (!use_bootinput)
