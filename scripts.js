20dedf2fea9a3e2fce10f3da67b5169dd57849d4
Sebastián Contreras <seba.contreras@outlook.com>
2017-06-21 05:14:17 +0000

    fix XL/LL issue

--- scripts.js
+++ scripts.js
@@ -151,9 +151,9 @@ $("#settings input, #settings select").on('change', function() {
 			break;
 		case 'n2DSXL':
 			if (region == 'JPN')
-				write(0, 16*5, 'New Nintendo 2DS XL JAN-001('+region+')');
-			else
 				write(0, 16*5, 'New Nintendo 2DS LL JAN-001('+region+')');
+			else
+				write(0, 16*5, 'New Nintendo 2DS XL JAN-001('+region+')');
 			processor = 4; sd += ' SD'
 			break;
 		case 'n3DS':
