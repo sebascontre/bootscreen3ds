65021219ac4b2bac71e0048390fac3ace0999077
SebasContre <sebascontre@users.noreply.github.com>
2025-03-06 01:22:22 -0300

    update for luma 13.3.1

--- scripts.js
+++ scripts.js
@@ -100,6 +100,10 @@ $("#settings input, #settings select").on('change', function() {
 			$topscreen.attr('width', 400);
 			line2 = 'Copyright(C) 2024, ' + line2;
 			break;
+		case 'luma2025':
+			$topscreen.attr('width', 400);
+			line2 = 'Copyright(C) 2025, ' + line2;
+			break;
 		case 'menuhax2015':
 			$topscreen.attr('width', 800);
 			line2 = 'Copyright(C) 2015, yellow8';
